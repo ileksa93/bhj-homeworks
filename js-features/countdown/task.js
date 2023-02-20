6348f4fefcb167549d6b2cd0a81d616c247b145c
ileksa <ileksababy@gmail.com>
2023-02-21 01:06:51 +0300

    wdfegtrhj

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,7 +1,10 @@
-let timer = 59; // стартовое значение таймера
+// JavaScript код в файле task.js
+const timerElement = document.getElementById("timer"); // получаем элемент, содержащий значение таймера
+let timer = parseInt(timerElement.textContent); // получаем начальное значение таймера и преобразуем его в число
 
 const intervalId = setInterval(() => {
   timer--; // уменьшаем значение таймера на 1
+  timerElement.textContent = timer; // обновляем значение таймера на странице
   if (timer === 0) {
     clearInterval(intervalId); // останавливаем таймер, когда достигнуто значение 0
     alert("Вы победили в конкурсе!");
